ca63f412cc7435bc567bdd56b23d504acee4f5b5
Dexcs.Mogura <dexcsof@gmail.com>
2026-04-21 11:22:30 +0900

    CfdOFを修正するパッチデータとスクリプトを同梱

---
+++ patchCfdOf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# FileManager の代わりに、TreeFoamを起動
+ln -s ../../dexcsCfdOF/Macro ../CfdOF/CfdOF/Macro
+patch -u ../CfdOF/CfdOF/CfdTools.py < CfdTools.patch
+
+#対流項スキームのデフォルトを1次風上に
+patch -u ../CfdOF/Data/Templates/case/system/fvSchemes < fvSchemes.patch
+#featureAngleを 120 → 85 に変更
+patch -u ../CfdOF/Data/Templates/mesh/system/snappyHexMeshDict < snappyHexMeshDict.patch
+
