0975ea26984d4b7cd19ef67e2be95509fc9afe2d
Dexcs.Mogura <dexcsof@gmail.com>
2026-04-21 17:47:22 +0900

    CfdOFでのparaview起動時の警告を回避

--- patchCfdOf.sh
+++ patchCfdOf.sh
@@ -6,6 +6,8 @@ patch -u ../CfdOF/CfdOF/CfdTools.py < CfdTools.patch
 
 #対流項スキームのデフォルトを1次風上に
 patch -u ../CfdOF/Data/Templates/case/system/fvSchemes < fvSchemes.patch
+#Paraview起動時のWarning回避
+patch -u ../CfdOF/Data/Templates/case/pvScript.py < pvScript.patch
 #featureAngleを 120 → 85 に変更
 patch -u ../CfdOF/Data/Templates/mesh/system/snappyHexMeshDict < snappyHexMeshDict.patch
 
